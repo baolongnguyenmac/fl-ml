db5d4b26878c6c1fc193080b427c2167534774aa
Nguyễn Bảo Long <nguyenbaolong@Nguyens-MacBook-Pro.local>
2021-09-20 16:02:03 +0700

    move train and test func from model to client

--- model/femnist_model.py
+++ model/femnist_model.py
@@ -2,12 +2,10 @@ import torch
 import torch.nn as nn
 
 import sys 
-sys.path.append('../')
-from strategy_client.conventional_ml import ConventionalTrain, ConventionalTest
+sys.path.insert(0, '../')
 
 class Femnist(nn.Module):
     def __init__(self) -> None:
-        super(Femnist, self).__init__()
         self.network = nn.Sequential(
             nn.Conv2d(in_channels=1, out_channels=32, kernel_size=5, stride=1, padding=2),
             nn.MaxPool2d(kernel_size=2), # batch: [#numOfElement, 64, 14, 14]
@@ -24,20 +22,3 @@ class Femnist(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = x.reshape(-1, 1, 28, 28)
         return self.network(x)
-
-''' test network
-    # DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-    # from data.dataloaders import femnist as dataloader
-    # loader, size = dataloader.get_loader('../data/femnist/test/0/support.pickle')
-
-    # model = Femnist()
-    # for x, y in loader:
-    #     print(x.shape)
-    #     print(y.shape)
-    #     outs = model(x)
-    #     print(outs.shape)
-    #     loss = nn.functional.cross_entropy(outs, y)
-    #     print(loss)
-    #     break
-'''
