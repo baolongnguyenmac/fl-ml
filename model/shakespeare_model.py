f18a1e49117bb9dc603d0ee7732274cc541d1c55
Nguyễn Bảo Long <nguyenbaolong@Nguyens-MacBook-Pro.local>
2021-09-20 16:58:41 +0700

    demo shakespeare model, not test yet

--- model/shakespeare_model.py
+++ model/shakespeare_model.py
@@ -1,103 +1,42 @@
-from collections import OrderedDict
-from typing import Tuple
+import sys 
+sys.path.insert(0, '../')
+from data.dataloaders.shakespeare import LEAF_CHARACTERS
 
 import torch
 import torch.nn as nn
-import flwr as fl
-
-DATA_ROOT = "../data/shakespeare"
 
 class Shakespeare(nn.Module):
-    def __init__(self) -> None:
-        super(Shakespeare, self).__init__()
-        self.network = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=5, stride=1, padding=2),
-            nn.MaxPool2d(kernel_size=2), # batch: [#numOfElement, 64, 14, 14]
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, stride=1, padding=2),
-            nn.MaxPool2d(kernel_size=2), # batch: [#numOfElement, 64, 7, 7]
-            
-            nn.Flatten(start_dim=1),
-            nn.Linear(in_features=64*7*7, out_features=2048),
-            nn.ReLU(),
-            nn.Softmax(dim=1)
+    def __init__(
+        self,
+        chars: str = LEAF_CHARACTERS,
+        seq_len: int = 80,
+        hidden_size: int = 256,
+        embedding_dim: int = 8,
+    ):
+        super().__init__()
+        self.dict_size = len(chars)
+        self.seq_len = seq_len
+        self.hidden_size = hidden_size
+
+        self.encoder = nn.Embedding(self.dict_size, embedding_dim)
+        self.lstm = nn.LSTM(
+            input_size=embedding_dim,
+            hidden_size=hidden_size,
+            num_layers=2,
+            batch_first=True,  # Notice batch is first dim now
         )
+        self.decoder = nn.Linear(self.hidden_size, self.dict_size)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.network(x)
+    def forward(self, sentence: torch.Tensor) -> torch.Tensor:
+        """Forwards sentence to obtain next character.
 
-    def get_weights(self) -> fl.common.Weights:
-        """Get model weights as a list of NumPy ndarrays."""
-        return [val.cpu().numpy() for _, val in self.state_dict().items()]
+        Args:
+            sentence (torch.Tensor): Tensor containing indices of characters
 
-    def set_weights(self, weights: fl.common.Weights) -> None:
-        """Set model weights from a list of NumPy ndarrays."""
-        state_dict = OrderedDict(
-            {k: torch.Tensor(v) for k, v in zip(self.state_dict().keys(), weights)}
-        )
-        self.load_state_dict(state_dict, strict=True)
-
-
-def load_model():
-    """Load Shakespeare model
-
-    Returns:
-        Shakespeare: an instance of class Shakespeare
-    """
-    return Shakespeare()
-
-
-def train(
-    net: Shakespeare,
-    trainloader: torch.utils.data.DataLoader,
-    epochs: int,
-    device: torch.device,  # pylint: disable=no-member
-) -> None:
-    """Train the network."""
-    # Define loss and optimizer
-    criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-    print(f"Training {epochs} epoch(s) w/ {len(trainloader)} batches each")
-
-    # Train the network
-    for epoch in range(epochs):  # loop over the dataset multiple times
-        running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
-            images, labels = data[0].to(device), data[1].to(device)
-
-            # zero the parameter gradients
-            optimizer.zero_grad()
-
-            # forward + backward + optimize
-            outputs = net(images)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-
-            # print statistics
-            running_loss += loss.item()
-            if i % 2000 == 1999:  # print every 2000 mini-batches
-                print("[%d, %5d] loss: %.3f" % (epoch + 1, i + 1, running_loss / 2000))
-                running_loss = 0.0
-
-
-def test(
-    net: Shakespeare,
-    testloader: torch.utils.data.DataLoader,
-    device: torch.device,  # pylint: disable=no-member
-) -> Tuple[float, float]:
-    """Validate the network on the entire test set."""
-    criterion = nn.CrossEntropyLoss()
-    correct = 0
-    total = 0
-    loss = 0.0
-    with torch.no_grad():
-        for data in testloader:
-            images, labels = data[0].to(device), data[1].to(device)
-            outputs = net(images)
-            loss += criterion(outputs, labels).item()
-            _, predicted = torch.max(outputs.data, 1)  # pylint: disable=no-member
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-    accuracy = correct / total
-    return loss, accuracy
+        Returns:
+            torch.Tensor: Vector encoding position of predicted character
+        """
+        encoded_seq = self.encoder(sentence)  # (batch, seq_len, embedding_dim)
+        _, (h_n, _) = self.lstm(encoded_seq)  # (batch, seq_len, hidden_size)
+        pred = self.decoder(h_n[-1])
+        return nn.functional.softmax(pred, dim=1)
