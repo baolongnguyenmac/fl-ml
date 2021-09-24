2d0c7ebc69253df4f3a55705af07859dde89edb3
Nguyễn Bảo Long <nguyenbaolong@Nguyens-MacBook-Pro.local>
2021-09-24 09:55:41 +0700

    remove some label, keep 53 labels

--- data/dataloaders/shakespeare.py
+++ data/dataloaders/shakespeare.py
@@ -24,16 +24,31 @@ class ShakespeareDataset(Dataset[XY]):
 
         self.characters: str = LEAF_CHARACTERS
         self.num_letters: int = len(self.characters)  # 80
+        self.label = LABEL
 
         with open(path_to_pickle, "rb") as open_file:
             data = pickle.load(open_file)
-            self.sentence = data["x"]
-            self.next_word = data["y"]
+            self.sentence, self.next_word = self.filter_label(data["x"], data["y"])
             self.index = data["idx"]
             self.char = data["character"]
 
     def filter_label(self, features, labels):
-        pass
+        """remove features that don't have labels in LABEL (53 chars)
+
+        Args:
+            features (list): list features
+            labels (list): list labels
+
+        Returns:
+            Tuple[list, list]: features and labels after filtering
+        """
+        new_features = []
+        new_labels = []
+        for i, item in enumerate(labels):
+            if item in LABEL:
+                new_features.append(features[i])
+                new_labels.append(item)
+        return new_features, new_labels
 
     def word_to_indices(self, word: str) -> List[int]:
         """Converts a sequence of characters into position indices in the
@@ -46,20 +61,12 @@ class ShakespeareDataset(Dataset[XY]):
         indices: List[int] = [self.characters.find(c) for c in word]
         return indices
 
-    def one_hot(self, index, size):
-        '''returns one-hot vector with given size and value 1 at given index
-        '''
-        vec = [0 for _ in range(size)]
-        vec[int(index)] = 1
-        return vec
-
     def __len__(self) -> int:
         return len(self.next_word)
 
     def __getitem__(self, idx: int) -> XY:
         sentence_indices = torch.tensor(self.word_to_indices(self.sentence[idx]))
-        # next_word_index = torch.tensor(self.one_hot(self.characters.find(self.next_word[idx]), len(LEAF_CHARACTERS)))
-        next_word_index = torch.tensor(self.characters.find(self.next_word[idx]))
+        next_word_index = torch.tensor(self.label.find(self.next_word[idx]))
         return sentence_indices, next_word_index
 
 def get_loader(path_to_pickle, batch_size=32, shuffle=True):
