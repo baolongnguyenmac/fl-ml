64149dc469f1b841dae4fce3f48e783cb4dc8611
Nguyễn Bảo Long <nguyenbaolong@Nguyens-MacBook-Pro.local>
2021-10-15 08:03:14 +0700

    run fedmeta-sgd

--- run-clients.sh
+++ run-clients.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-NUM_CLIENTS=5
+# - strategy:
+#     - FedAvg
+#     - FedMetaMAML
+#     - FedAvgMeta
+#     - FedMetaSGD
+
+# - model:
+#     - femnist
+#     - shakespeare
+#     - sent140
+
+NUM_CLIENTS=20
 
 echo "Starting $NUM_CLIENTS clients."
 for ((i = 0; i < $NUM_CLIENTS; i++))
 do
     echo "Starting client(cid=$i) with partition $i out of $NUM_CLIENTS clients."
     python3 -m client.client \
-      --cid=$i \
-      --strategy='FedMetaSGD' \
-      --alpha=0.1 \
-      --model='shakespeare' &s
+        --cid=$i \
+        --strategy='FedMetaSGD' \
+        --alpha=0.1 \
+        --model='femnist' &
 done
 echo "Started $NUM_CLIENTS clients."
 
--- run-server.sh
+++ run-server.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-# Start a Flower server
-# python3 -m server.server \
-#   --rounds=10 \
-#   --epochs=1 \
-#   --sample_fraction=0.3 \
-#   --min_sample_size=1 \
-#   --min_num_clients=1 \
-#   --strategy='FED_AVG'  \
-#   --learning_rate=0.001
+# - strategy:
+#     - FED_AVG
+#     - FED_META_MAML
+#     - FED_META_SGD
 
+# # fedAvg stuff
+# python3 -m server.server \
+#     --rounds=3 \
+#     --epochs=2 \
+#     --sample_fraction=0.3 \
+#     --min_sample_size=2 \
+#     --min_num_clients=2 \
+#     --strategy='FED_AVG' \
+#     --learning_rate=0.001
+
+# meta learning based
 python3 -m server.server \
-  --rounds=10 \
-  --epochs=1 \
-  --sample_fraction=0.3 \
-  --min_sample_size=1 \
-  --min_num_clients=1 \
-  --strategy='FED_META_SGD'  \
-  --alpha=0.01 \
-  --beta=0.1
+    --rounds=5 \
+    --epochs=2 \
+    --sample_fraction=0.3 \
+    --min_sample_size=2 \
+    --min_num_clients=2 \
+    --strategy='FED_META_SGD' \
+    --alpha=0.01 \
+    --beta=10
