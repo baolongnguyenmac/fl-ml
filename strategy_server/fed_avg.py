fbad0cf112aabaf86c8381db871a124fe6609dd1
caotatcuong <cuong091200@gmail.com>
2021-10-17 18:12:22 +0700

    Add accuracy to evaluate results

--- strategy_server/fed_avg.py
+++ strategy_server/fed_avg.py
@@ -1,15 +1,73 @@
 from logging import WARNING
-from typing import List, Tuple
-from flwr.common import FitIns,Parameters,parameters_to_weights
+from typing import Dict, List, Optional, Tuple
+
+from flwr.common import (
+    EvaluateRes,
+    FitRes,
+    Parameters,
+    Scalar,
+    parameters_to_weights,
+    weights_to_parameters,
+)
+from flwr.server.strategy.aggregate import aggregate
+
 from flwr.server.client_manager import ClientManager
 from flwr.server.client_proxy import ClientProxy
-from flwr.server.strategy import FedAvg
+from flwr.server.strategy.fedavg import FedAvg
 
 import numpy as np
 np.warnings.filterwarnings('ignore', category=np.VisibleDeprecationWarning)
 
+def weighted_loss_acc_avg(results: List[Tuple[int, float, Optional[float]]]) -> float:
+    """Aggregate evaluation results obtained from multiple clients."""
+    num_total_evaluation_examples = sum(
+        [num_examples for num_examples, _, _ in results]
+    )
+    weighted_losses = [num_examples * loss for num_examples, loss, _ in results]
+    weighted_acc = [num_examples * acc for num_examples, _, acc in results]
+
+    return sum(weighted_losses) / num_total_evaluation_examples, sum(weighted_acc) / num_total_evaluation_examples
 class MyFedAvg(FedAvg):
-    def configure_fit(self, rnd: int, parameters: Parameters, client_manager: ClientManager) -> List[Tuple[ClientProxy, FitIns]]:
-        weight = parameters_to_weights(parameters)
-        print('\n\n', weight, '\n\n')
-        return super().configure_fit(rnd, parameters, client_manager)
+
+    def aggregate_fit(
+        self,
+        rnd: int,
+        results: List[Tuple[ClientProxy, FitRes]],
+        failures: List[BaseException],
+    ) -> Tuple[Optional[Parameters], Dict[str, Scalar]]:
+        """Aggregate fit results using weighted average."""
+        if not results:
+            return None, {}
+        # Do not aggregate if there are failures and failures are not accepted
+        if not self.accept_failures and failures:
+            return None, {}
+        # Convert results
+        weights_results = [
+            (parameters_to_weights(fit_res.parameters), fit_res.num_examples)
+            for client, fit_res in results
+        ]
+        return weights_to_parameters(aggregate(weights_results)), {}
+
+    def aggregate_evaluate(
+        self,
+        rnd: int,
+        results: List[Tuple[ClientProxy, EvaluateRes]],
+        failures: List[BaseException],
+    ) -> Tuple[Optional[float], Dict[str, Scalar]]:
+        """Aggregate evaluation losses using weighted average."""
+        if not results:
+            return None, {}
+        # Do not aggregate if there are failures and failures are not accepted
+        if not self.accept_failures and failures:
+            return None, {}
+        loss_aggregated, acc_aggregated = weighted_loss_acc_avg(
+            [
+                (
+                    evaluate_res.num_examples,
+                    evaluate_res.loss,
+                    evaluate_res.metrics['acc'],
+                )
+                for _, evaluate_res in results
+            ]
+        )
+        return loss_aggregated, {"accuracy": acc_aggregated}
