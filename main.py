f51f46639f5dde582e0ba3d5575a46abac7a5563
caotatcuong <cuong091200@gmail.com>
2021-10-30 17:05:33 +0700

    Fix run on gpu

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ def main():
     fl.simulation.start_simulation(
         client_fn=client_fn_config(args),
         num_clients=args.num_clients,
-        # client_resources={"num_cpus": 4},
+        client_resources={"num_cpus": 2, "num_gpus": 1},
         num_rounds=args.rounds,
         strategy=MyFedAvg(
             fraction_fit=args.fraction_fit,
