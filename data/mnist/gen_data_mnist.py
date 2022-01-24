4f469e74d60e46952563afbbd3f29eb1e8f8e9fb
Nguyễn Bảo Long <nguyenbaolong@Nguyens-MacBook-Pro.local>
2022-01-24 10:35:37 +0700

    generate new client

--- data/mnist/gen_data_mnist.py
+++ data/mnist/gen_data_mnist.py
@@ -9,7 +9,7 @@ NUM_LABEL_PER_CLIENT = 2
 NUM_LABEL = 10
 INTERVALS = int(NUM_CLIENT/NUM_LABEL*NUM_LABEL_PER_CLIENT)
 
-def main():
+def gen_client():
     # read data from file
     df_test: pd.DataFrame = pd.read_csv('./mnist_test.csv')
     df_train: pd.DataFrame = pd.read_csv('./mnist_train.csv')
@@ -55,7 +55,7 @@ def split_num_sample_into_intervals(data_by_label: dict):
 #            -> "num_samples": list
 #            -> "user_data": dict -> "user_id": dict -> x: (num_samples, 784)
 #                                                    -> y: (num_samples,)
-def write_to_file(client_dict: dict):
+def write_to_file(client_dict: dict, new_client=False):
     all_train_data = {}
     all_test_data = {}
 
@@ -76,15 +76,19 @@ def write_to_file(client_dict: dict):
         test_user_data[user]['x'] = []
         test_user_data[user]['y'] = []
 
-        for key in client_dict[user].keys():
-            len_x = len(client_dict[user][key])
-            p = int(0.75*len_x)
+        for label in client_dict[user].keys():
+            len_x = len(client_dict[user][label])
 
-            train_user_data[user]['x'].extend(client_dict[user][key][:p])
-            train_user_data[user]['y'].extend([key]*p)
+            if new_client:
+                test_user_data[user]['x'].extend(client_dict[user][label])
+                test_user_data[user]['y'].extend([label]*len_x)
+            else:
+                p = int(0.75*len_x)
+                train_user_data[user]['x'].extend(client_dict[user][label][:p])
+                train_user_data[user]['y'].extend([label]*p)
 
-            test_user_data[user]['x'].extend(client_dict[user][key][p:])
-            test_user_data[user]['y'].extend([key]*(len_x - p))
+                test_user_data[user]['x'].extend(client_dict[user][label][p:])
+                test_user_data[user]['y'].extend([label]*(len_x - p))
 
         num_train_samples.append(len(train_user_data[user]['y']))
         num_test_samples.append(len(test_user_data[user]['y']))
@@ -94,10 +98,17 @@ def write_to_file(client_dict: dict):
     all_train_data['user_data'] = train_user_data
     all_test_data['user_data'] = test_user_data
 
-    with open('./train_json/train.json', 'w') as outfile:
-        json.dump(all_train_data, outfile)
-    with open('./test_json/test.json', 'w') as outfile:
-        json.dump(all_test_data, outfile)
+    if not new_client:
+        with open('./train_json/train.json', 'w') as outfile:
+            json.dump(all_train_data, outfile)
+            outfile.close()
+        with open('./test_json/test.json', 'w') as outfile:
+            json.dump(all_test_data, outfile)
+            outfile.close()
+    else:
+        with open('./test_json/new_test.json', 'w') as outfile:
+            json.dump(all_test_data, outfile)
+            outfile.close()
 
 def split_data_into_classes(X, y):
     all_data = {}
@@ -107,31 +118,28 @@ def split_data_into_classes(X, y):
         all_data[item].append(X[i].tolist())
     return all_data
 
-def split_data_into_clients(data_by_label, num_sample_in_label: list):
-    # divide all_data into clients
-    np.random.seed(10)
+# divide all_data into clients
+def split_data_into_clients(data_by_label, num_sample_in_label: list, labels = list(range(10))):
     all_user = {}
-    flag1 = [0]*NUM_LABEL
-    flag2 = [0]*NUM_LABEL
-    available_label = set(list(range(10)))
+    flag1 = {labels[i]: 0 for i in labels} # trace a label
+    flag2 = {labels[i]: 0 for i in labels} # trace interval in a label
 
     for i in range(NUM_CLIENT):
         all_user[str(i)] = {}
+        idx = [i%10, i%10 + 1 if i%10 != 9 else 0]
+        label_of_client = [labels[t] for t in idx]
 
-        labels = np.random.choice(a=list(available_label), size=(2,), replace=False)
-        for label in labels:
+        for label in label_of_client:
             tmp = flag1[label]
             tmp_ = tmp + num_sample_in_label[label][flag2[label]]
-            all_user[str(i)][label] = data_by_label[label][tmp:tmp_]
+            all_user[str(i)][int(label)] = data_by_label[label][tmp:tmp_]
             flag1[label] = tmp_
             flag2[label] += 1
-            if flag2[label] == INTERVALS:
-                available_label.remove(label)
     return all_user
 
 def check(all_client):
     fig, axes = plt.subplots(nrows=5, ncols=10, figsize=(7, 7), sharey=True)
-    fig.subplots_adjust(hspace=0.2)
+    fig.subplots_adjust(hspace=0.5)
 
     for i, key in enumerate(all_client.keys()):
         print(key, len(all_client[key]))
@@ -141,10 +149,53 @@ def check(all_client):
 
         x = list(all_client[key].keys())
         y = [len(all_client[key][t]) for t in all_client[key].keys()]
-        axes[int(i/10), int(i % 10)].bar(x=x, height=y)
-        # axes[int(i/10), int(i%10)].set_title(len(all_client[key]['y']))
+        axes[i//10, i%10].bar(x=x, height=y)
+        axes[i//10, i%10].set_title(f'{sum(y)}, {list(all_client[key].keys())}')
+        axes[i//10, i%10].set_xlim(-1,10)
 
     plt.show()
 
+# read file test in *.json and wrap in a np.array
+def get_test_data():
+    with open('./test_json/pFedMe_mnist_test.json', 'r') as input_file:
+        data = json.load(input_file)
+        input_file.close()
+
+    all_data = []
+    for user in data['user_data'].keys():
+        feature = np.array(data['user_data'][user]['x'])
+        label = np.array(data['user_data'][user]['y']).reshape(-1, 1)
+        c = np.concatenate((label, feature), axis=1)
+        all_data.extend(c)
+
+    all_data = np.array(all_data)
+    np.random.shuffle(all_data)
+    return all_data
+
+def gen_new_test_client():
+    data = get_test_data()
+    print(data.shape)
+
+    # split into x, y
+    X = data[:, 1:]
+    y = data[:, 0]
+
+    # divide data (X, y) into classes
+    data_by_label = split_data_into_classes(X, y)
+
+    # divide num_sample of each class into intervals
+    num_sample_in_label = split_num_sample_into_intervals(data_by_label)
+
+    # divide data_by_label into client
+    labels = list(range(10))
+    np.random.shuffle(labels)
+    client_dict = split_data_into_clients(data_by_label, num_sample_in_label, labels)
+
+    # write to file
+    write_to_file(client_dict, new_client=True)
+
+    check(client_dict)
+
 if __name__ == '__main__':
-    main()
+    # gen_client()
+    gen_new_test_client()
