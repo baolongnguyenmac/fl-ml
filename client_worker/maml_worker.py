3d61b68870a59be31c48a65bfe00a66e821464ea
Nguyễn Bảo Long <nguyenbaolong@Nguyens-MacBook-Pro.local>
2022-01-24 23:52:28 +0700

    add best_test method and fix shit error for ensemble test

--- client_worker/maml_worker.py
+++ client_worker/maml_worker.py
@@ -69,7 +69,6 @@ class MAMLWorker(BaseWorker):
 
         support_loader, _ = self.get_loader(support=True, train=False, model_name=model_wrapper.model_name, batch_size=batch_size)
         query_loader, num_query_sample = self.get_loader(support=False, train=False, model_name=model_wrapper.model_name, batch_size=batch_size)
-        learner:MAML = model_wrapper.model.module.clone()
         num_model = len(os.listdir('./personalized_weight'))
 
         print(f'[Client {self.cid}]: Evaluate {epochs} epoch(s) on {len(support_loader)} batch(es) using {self.device}')
@@ -79,6 +78,7 @@ class MAMLWorker(BaseWorker):
         for i in range(num_model):
             # load weight and fine tune local model
             model_wrapper.load_personalization_weight(i, weights, per_layer)
+            learner:MAML = model_wrapper.model.module.clone()
             for e in range(epochs):
                 for batch in support_loader:
                     loss, _ = self._training_step(learner, batch)
@@ -87,7 +87,7 @@ class MAMLWorker(BaseWorker):
             loss = 0. # loss of a model in ensemble schema
             prob = [] # prob (output) array of a client
             for batch in query_loader:
-                tmp_loss, tmp_prob, label = self._valid_step(model_wrapper.model, batch, return_prob=True)
+                tmp_loss, tmp_prob, label = self._valid_step(learner, batch, return_prob=True)
                 loss += tmp_loss
                 prob.append(tmp_prob)
 
@@ -110,6 +110,58 @@ class MAMLWorker(BaseWorker):
         _, preds = torch.max(probs, dim=1)
         labels = torch.cat(labels, dim=0)
         val_acc = (preds == torch.tensor(labels)).sum() / num_query_sample
+
+        print(f'[Client {self.cid}]: Val loss = {float(val_loss)}, Val acc = {float(val_acc)}')
+        return float(val_loss), float(val_acc), num_query_sample
+
+    def best_test(self, model_wrapper:ModelWrapper, batch_size:int, epochs:int, current_round:str, weights:Weights, per_layer:int):
+        print(f'[Client {self.cid}]: Get best eval in round {current_round}')
+
+        support_loader, _ = self.get_loader(support=True, train=False, model_name=model_wrapper.model_name, batch_size=batch_size)
+        query_loader, num_query_sample = self.get_loader(support=False, train=False, model_name=model_wrapper.model_name, batch_size=batch_size)
+        num_model = len(os.listdir('./personalized_weight'))
+
+        print(f'[Client {self.cid}]: Evaluate {epochs} epoch(s) on {len(support_loader)} batch(es) using {self.device}')
+
+        # find the per-layer that fit best to the new data (produce min loss)
+        losses = []
+        for i in range(num_model):
+            # load weight and fine tune local model
+            model_wrapper.load_personalization_weight(i, weights, per_layer)
+            learner:MAML = model_wrapper.model.module.clone()
+            for e in range(epochs):
+                finetune_loss = 0.
+                for batch in support_loader:
+                    loss, _ = self._training_step(learner, batch)
+                    learner.adapt(loss)
+
+                    finetune_loss += loss
+
+            finetune_loss /= len(support_loader)
+            losses.append(finetune_loss)
+
+        # get min loss index
+        min_idx = min(range(len(losses)), key = lambda i: losses[i])
+
+        # load min-loss weight and fine tune local model
+        model_wrapper.load_personalization_weight(min_idx, weights, per_layer)
+        learner:MAML = model_wrapper.model.module.clone()
+        for e in range(epochs):
+            finetune_loss = 0.
+            for batch in support_loader:
+                loss, _ = self._training_step(learner, batch)
+                learner.adapt(loss)
+
+        # test the model
+        val_loss = 0.
+        val_acc = 0.
+        for batch in query_loader:
+            loss, acc = self._valid_step(learner, batch)
+            val_loss += loss
+            val_acc += acc
+
+        val_loss /= len(query_loader)
+        val_acc /= num_query_sample
 
         print(f'[Client {self.cid}]: Val loss = {float(val_loss)}, Val acc = {float(val_acc)}')
         return float(val_loss), float(val_acc), num_query_sample
