cf2afd961cd8f858b4af22f759acfad6a3aa3e02
Nguyễn Bảo Long <nguyenbaolong@Nguyens-MacBook-Pro.local>
2022-01-24 10:36:58 +0700

    add save_weight and load_weight

--- model/model_wrapper.py
+++ model/model_wrapper.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 from flwr.common import Weights
 from collections import OrderedDict
 from learn2learn.algorithms.meta_sgd import MetaSGD, meta_sgd_update, clone_module, clone_parameters
+import pickle
 
 FED_AVG = "FedAvg"
 FED_META_MAML = "FedMetaMAML"
@@ -33,6 +34,59 @@ class ModelWrapper:
             {k: torch.Tensor(v) for k, v in zip(self.model.state_dict().keys(), weights)}
         )
         self.model.load_state_dict(state_dict, strict=True)
+
+    def save_personalization_weight(self, cid:str, per_layer:int, meta_sgd=False):
+        weights = self.get_weights()
+
+        if meta_sgd:
+            num_weight = len(weights) # ex: 3layer -> num_weight = 12 (3 weight, 3 bias, 6 alpha)
+
+            # save weight
+            personalized_weight = weights[-num_weight//2 + per_layer:-num_weight//2]
+            with open(f'./personalized_weight/{cid}.pickle', 'wb') as file_weight:
+                pickle.dump(personalized_weight, file_weight)
+            file_weight.close()
+
+            # save alpha (is contained by weights)
+            personalized_alpha = weights[per_layer:]
+            with open(f'./personalized_weight/{cid}_alpha.pickle', 'wb') as file_alpha:
+                pickle.dump(personalized_alpha, file_alpha)
+            file_alpha.close()
+        else:
+            personalized_weight = weights[per_layer:]
+            with open(f'./personalized_weight/{cid}.pickle', 'wb') as file_input:
+                pickle.dump(personalized_weight, file_input)
+            file_input.close()
+
+    def load_personalization_weight(self, cid:str, weights:Weights, per_layer:int, meta_sgd=False):
+        try: # in case the file do not exist
+            if meta_sgd:
+                # load local weight from file
+                num_weight = len(weights) # ex: 3layer -> num_weight = 12 (3 weight, 3 bias, 6 alpha)
+                with open(f'./personalized_weight/{cid}.pickle', 'rb') as file_weight:
+                    personalized_weight = pickle.load(file_weight)
+                    file_weight.close()
+                weights[-num_weight//2 + per_layer:-num_weight//2] = personalized_weight
+                file_weight.close()
+
+                # load alpha
+                with open(f'./personalized_weight/{cid}_alpha.pickle', 'rb') as file_alpha:
+                    personalized_alpha = pickle.load(file_alpha)
+                    file_alpha.close()
+                weights[per_layer:] = personalized_alpha
+                file_alpha.close()
+            else:
+                # load local weight from file
+                with open(f'./personalized_weight/{cid}.pickle', 'rb') as file_input:
+                    personalized_weight = pickle.load(file_input)
+                    file_input.close()
+                # assign local weight to the current weight
+                weights[per_layer:] = personalized_weight
+        except:
+            pass
+
+        # set new weight to the model
+        self.set_weights(weights)
 
 class MetaSGDModelWrapper(MetaSGD):
     """generate a meta model that wraps a nn.Module
